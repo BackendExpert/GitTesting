b24ee599a8ebfd35dfe9dedb9b3a3013eebcdc98
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-06 10:54:30 +0530

    Update App.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AddFile from "./comportments/FileUpload/AddFile";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AddFile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }

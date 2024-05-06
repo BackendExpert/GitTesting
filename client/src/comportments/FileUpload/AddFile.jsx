b1a13953c2fce57468d8a40765f2ba02e828b6b4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-06 10:56:43 +0530

    Update AddFile.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- client/src/comportments/FileUpload/AddFile.jsx
+++ client/src/comportments/FileUpload/AddFile.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
+
 const AddFile = () => {
   return (
-    <div>AddFile</div>
+    <div>
+        <div className="my-8 px-16">
+            <div className="">
+                <form>  
+                    <div className="lg:grid grid-cols-3 gap-4">
+                        <div className="my-2">
+                            <label htmlFor=""></label>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
   )
 }
 
